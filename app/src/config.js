3efaa879627e3a0b9e7a5f27ff763a2102393f4b
Roman-G-579 <romangury579@gmail.com>
2024-03-17 12:52:04 +0200

    updated baseurl

--- app/src/config.js
+++ app/src/config.js
@@ -1,2 +1,3 @@
 // export const baseUrl = "http://localhost:5050";
-export const baseUrl = "https://mongodb-express-rest-api-example.vercel.app";
+// export const baseUrl = "https://mongodb-express-rest-api-example.vercel.app";
+export const baseUrl = "https://mongodb-express-rest-api-example-production.up.railway.app"
