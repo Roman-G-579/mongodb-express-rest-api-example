82ea358f872eddaa59f65dbafb8c033831ac3317
Roman-G-579 <romangury579@gmail.com>
2024-03-17 12:14:27 +0200

    changed baseUrl

--- app/src/config.js
+++ app/src/config.js
@@ -1,2 +1,2 @@
-export const baseUrl = "http://localhost:5050";
-// export const baseUrl = "https://mongodb-express-rest-api-example.vercel.app";
+// export const baseUrl = "http://localhost:5050";
+export const baseUrl = "https://mongodb-express-rest-api-example.vercel.app";
